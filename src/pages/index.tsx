195238264a2d53e8d053a023406a2c89a74ca8d6
csainath07 <iamsainath07@gmail.com>
2020-10-11 11:34:47 +0530

    feat: meta data added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,45 @@ const Home: FC = () => {
   return (
     <div>
       <Layout
-        title="Home"
+        title="Home | FootPrints Tech"
         meta={[
           {
-            property: 'og:description',
-            content: ''
+            property: 'title',
+            content: 'FootPrints Tech'
           },
           {
-            property: 'og.keywords',
-            content: ''
+            property: 'image',
+            content:
+              'https://res.cloudinary.com/dfiywgv6u/image/upload/v1596086104/footprintstech_cbajxc.png'
+          },
+          {
+            property: 'description',
+            content:
+              'Get digital solution for your idea, Honest, righteous and to the point defines us, defines FootPrints Tech.'
+          },
+          {
+            property: 'keywords',
+            content:
+              'app, website, development, app development, website development'
+          },
+          {
+            property: 'og:title',
+            content: 'FootPrints Tech'
+          },
+          {
+            property: 'og:image',
+            content:
+              'https://res.cloudinary.com/dfiywgv6u/image/upload/v1596086104/footprintstech_cbajxc.png'
+          },
+          {
+            property: 'og:description',
+            content:
+              'Get digital solution for your idea, Honest, righteous and to the point defines us, defines FootPrints Tech.'
+          },
+          {
+            property: 'og:keywords',
+            content:
+              'app, website, development, app development, website development'
           }
         ]}
       >
